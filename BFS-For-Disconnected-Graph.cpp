7c6ba8a7e8fe2eddd3dba72faff31ff6587e5ce7
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-09 21:58:31 +0530

    Add files via upload

---
+++ BFS-For-Disconnected-Graph.cpp
@@ -0,0 +1,69 @@
+// Program to Find BFS Traversal of Disconnected Graph
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<vector<int>>&AdjList, int u, int v){
+	AdjList[u].push_back(v);
+}
+
+void printGraph(vector<vector<int>>&AdjList, int V){
+	for(int i=0;i<V;i++){
+		cout<<i<<"=>";
+		for(auto it:AdjList[i])
+			cout<<it<<" ";
+		cout<<"\n";
+	}
+	return;
+}
+
+void BFSUtil(vector<vector<int>>&AdjList, bool *visited, int root){
+	
+	visited[root]=true;
+	queue<int>q;
+	q.push(root);
+	while(!q.empty()){
+		int temp=q.front();
+		cout<<temp<<" ";
+		q.pop();
+		for(auto it:AdjList[temp]){
+			if(visited[it]==false){
+				visited[it]=true;
+				q.push(it);
+			}
+		}
+	}
+	return;
+}
+
+void BFSTraversal(vector<vector<int>>&AdjList, int V){
+
+	bool *visited=new bool[V];
+	memset(visited,false,sizeof(visited));
+
+	for(int i=0;i<V;i++){
+		if(visited[i]==false)
+			BFSUtil(AdjList,visited,i);
+	}
+	return;
+}
+
+int main(){
+
+	int V=4;
+	vector<vector<int>>AdjList(V);
+
+	addEdge(AdjList,0,2);
+	addEdge(AdjList,2,0);
+	addEdge(AdjList,0,1);
+	addEdge(AdjList,2,3);
+	addEdge(AdjList,3,3);
+
+	printGraph(AdjList,V);
+	cout<<"\n";
+
+	BFSTraversal(AdjList,V);
+
+
+	return 0;
+}
