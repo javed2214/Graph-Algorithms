d47e15bc4ab934cca4caf457021609e9b762e5e8
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-08-01 00:53:05 +0530

    Add files via upload

---
+++ Print-Graph-Cycle.cpp
@@ -0,0 +1,60 @@
+// Program to Print Cycles of Graph
+
+#include<bits/stdc++.h>
+using namespace std;
+#define MX 101
+
+vector<int> g[MX];
+int vis[MX];
+int parent[MX];
+
+int fam,cam;
+
+bool dfs(int u, int par){
+
+	vis[u] = 1;	
+
+	for(auto it:g[u]){
+		if(!vis[it]){
+			parent[it] = u;
+			if(dfs(it,u))
+				return true;
+		}
+		else if(it != par){
+			fam = it;
+			cam = u;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+int main(){
+
+	int n, m;
+	cin>>n>>m;
+
+	for(int i=0;i<m;i++){
+		int u,v;
+		cin>>u>>v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+
+	bool x = dfs(1,-1);
+
+	if(x) cout<<"\nCycle is Present";
+	else cout<<"\nNot Present!";
+
+	cout<<endl;
+
+	while(true){
+		cout<<cam<<" ";
+		cam = parent[cam];
+		if(cam == fam) break;
+	}
+	cout<<cam;
+
+	return 0;
+}
